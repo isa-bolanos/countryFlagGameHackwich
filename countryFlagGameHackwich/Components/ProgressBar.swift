6311890c728cd68572e7f0ac10713b6defe521c2
isa-bolanos <isa.bolanos55@gmail.com>
2023-01-19 10:11:15 -0600

    Created initial view

--- countryFlagGameHackwich/Components/ProgressBar.swift
+++ countryFlagGameHackwich/Components/ProgressBar.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ProgressBar: View {
-    var progress: CG
+    var progress: CGFloat
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading) {
+            Rectangle()
+                .frame(maxWidth: 350, maxHeight: 4)
+                .foregroundColor(.gray)
+                .cornerRadius(10)
+            Rectangle()
+                .frame(width: progress, height: 4)
+                .foregroundColor(.yellow)
+                .cornerRadius(10)
+        }
     }
 }
 
 struct ProgressBar_Previews: PreviewProvider {
     static var previews: some View {
-        ProgressBar()
+        ProgressBar(progress: 50)
     }
 }
